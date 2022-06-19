83ddf2d4467d9811120b032541113229808a29c7
Chandandeep Singh <singhchandandeep34@gmail.com>
2022-06-19 15:49:00 +0200

    A2.1, A2.2 and A2.3

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import cv2
 import mediapipe as mp
 import time
 import os
+
+cap = cv2.VideoCapture('InputVideo/input.mp4')
+while True:
+    success, img = cap.read()
+    cv2.waitKey(1)
+    cv2.imshow("Image", img)
+    if(cv2.waitKey(1) & 0xFF==ord('q')):
+         break;
